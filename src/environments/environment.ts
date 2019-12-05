9f34c0c94805c92b6b2dbc4e9efced3dcbbb14e3
rom <penaflor024@gmail.com>
2019-12-06 01:27:39 +0800

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx"
+  }
 };
 
 /*
